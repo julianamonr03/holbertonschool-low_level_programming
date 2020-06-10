d2e604a311bf09dd4cbf42901aae94ecceafb472
julianamonr03 <1967@holbertonschool.com>
2020-06-10 23:47:24 +0000

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * puts:Programming is like building a multilingual puzzle 
+ * Return: Alwasy 0
+ */
+int main(void)
+{
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
+}
