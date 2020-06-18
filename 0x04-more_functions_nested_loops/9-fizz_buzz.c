a21c9c5a20179bb75332d399f9b72055e98bf126
julianamonr03 <1967@holbertonschool.com>
2020-06-18 20:58:17 +0000

    Fizz Buzz

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ *
+ *
+ *
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 1; n < 100; n++)
+	{
+		if (n % 3 == 0)
+		{
+		printf("Fizz ");
+		}
+		else if (n % 5 ==0)
+		{
+			printf("Buzz ");
+		}
+		else if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+	printf("Buzz");
+	printf("\n");
+	return (0);
+}
