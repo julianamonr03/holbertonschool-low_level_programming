7c7e65fef73831377c2ed667ae2f4c5dd967fb54
julianamonr03 <1967@holbertonschool.com>
2020-06-23 19:57:53 +0000

    Half of string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * puts_half - Entry point
+ *@str: half string
+ * Return: no return
+ */
+void puts_half(char *str)
+{
+	int a;
+
+	for (a = 0; str[a] != '\0'; a++)
+	{
+	}
+	for (a = a / 2; str[a] != '\0'; a++)
+	{
+		_putchar(str[a]);
+	}
+	_putchar('\n');
+}
