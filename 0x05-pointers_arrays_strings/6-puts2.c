917331572c185d499a55c72c849902186a1f741f
julianamonr03 <1967@holbertonschool.com>
2020-06-23 18:45:36 +0000

    Every other character

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * puts2 - Entry point
+ *@str: character of string
+ * Return: no return
+ */
+void puts2(char *str)
+{
+	int a = 0;
+
+	while (str[a] != '0')
+	{
+		if (a % 2 == 0)
+		{
+			_putchar(str[a]);
+		}
+		a++;
+	}
+	_putchar('\n');
+}
