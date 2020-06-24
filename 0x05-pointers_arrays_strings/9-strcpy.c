a77ca86aa2cc22d5aec10464e25a3aaad73aa0ed
julianamonr03 <1967@holbertonschool.com>
2020-06-24 01:44:47 +0000

    Copy a string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _strcpy - Entry point
+ *@dest: string dest
+ *@src: string src
+ * Return: dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; src[i]; i++)
+	{
+		dest[i] = src[i];
+	}
+		dest[i] = src[i];
+		return (dest);
+}
