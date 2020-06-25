3ff66f0c71e23979cdf78df40ba95eb165a5940c
julianamonr03 <1967@holbertonschool.com>
2020-06-25 23:30:15 +0000

    Reverse an array

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,12 +9,14 @@ void reverse_array(int *a, int n)
 {
 	int i = 0;
 	int j;
+	n = n - 1;
 
 	while (i < n)
 	{
 		j = a[i];
-		a[i] = a[n - 1];
-		a[n - 1] = j;
+		a[i] = a[n];
+		a[n] = j;
 		i++;
+		n--;
 	}
 }
