13919b115f435943e6dd1447f34600fa96a3abec
julianamonr03 <1967@holbertonschool.com>
2020-06-25 23:29:21 +0000

    Compare two strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 /**
- *
- *
- *
+ * _strcmp - Compares two strings
+ *@s1: String 1
+ *@s2: String 2
+ * Return: return to compare strings
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -10,12 +11,13 @@ int _strcmp(char *s1, char *s2)
 	int a = 0;
 	int b;
 
-	while (s1[a] || s2 [a])
+	while (s1[a] || s2[a])
 	{
 		if (s1[a] == s2[a])
 		{
 			b = 0;
 		}
+
 		else
 		{
 			b = s1[a] - s2[a];
