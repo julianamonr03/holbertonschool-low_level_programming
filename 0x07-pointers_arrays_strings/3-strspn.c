3be3dd18e2749469805be273ce20eb5ea5ed9b91
julianamonr03 <1967@holbertonschool.com>
2020-06-30 00:05:20 +0000

    gets lenght of a prefix substring

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 /**
  * _strspn - Gets lenght of a prefix
- *
- *
+ * @s: String
+ * @accept: Bytes to count in s
+ * Return: number of bytes in the initial segment
  */
 unsigned int _strspn(char *s, char *accept)
 {
