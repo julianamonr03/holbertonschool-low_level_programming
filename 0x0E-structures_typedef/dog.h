8d5e1259f09f8dd5be6980b8f72a1d9e0ce43048
julianamonr03 <1967@holbertonschool.com>
2020-07-13 19:39:06 +0000

    Django

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef _DJANGO_DOG_
+#define _DJANGO_DOG_
+/**
+ * struct dog - django dog
+ * @char: dog name
+ * @float: dog age
+ * @char: dog owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif /* _DJANGO_DOG_ */
