4a56a3626f3780d07416b1a50bf98caf252e9c8c
julianamonr03 <1967@holbertonschool.com>
2020-07-17 20:33:08 +0000

    Sum them all betty fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,9 +1,9 @@
 #include "variadic_functions.h"
 #include <stdio.h>
 /**
- *
- *
- *
+ * sum_them_all - Function that returns the sum of all its parameters
+ * @n: Numbers
+ * Return: sum of the elements
  */
 int sum_them_all(const unsigned int n, ...)
 {
@@ -14,8 +14,8 @@ int sum_them_all(const unsigned int n, ...)
 	va_start(arg, n);
 
 	if (n == 0)
-        {
-                return (0);
+	{
+		return (0);
 	}
 
 	for (i = 0; i < n; i++)
