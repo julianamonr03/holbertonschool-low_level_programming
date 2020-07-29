45771dab49d0f3bb56e547ef427721d3b2c7ca19
julianamonr03 <1967@holbertonschool.com>
2020-07-29 00:40:19 +0000

    More singly linked lists project

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+/**
+ * print_listint - Function that prints all the elements
+ * @h: list given
+ * Return: Numbers of nodes
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t cont_n= 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next, cont_n++;
+	}
+	return (cont_n);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,5 @@
+# In this part we are gonna learn more about singly linked lists
+----
+0x13.C More singly Linked Lists
+----
+*Made by Juliana Monroy*
