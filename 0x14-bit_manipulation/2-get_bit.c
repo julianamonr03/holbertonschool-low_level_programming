23b287af81f7ab6bd16a406a2d742d8633d2f753
julianamonr03 <1967@holbertonschool.com>
2020-07-31 01:14:43 +0000

    first commit 2nd

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+/**
+ * get_bit - Function that returns the value of a bit at a given index
+ * @n: int
+ * @index: index starting from 0
+ * Return: returns the value of a bit at a given index
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int i;
+
+	i = (n >> index) & 1;
+
+	return (i);
+}
