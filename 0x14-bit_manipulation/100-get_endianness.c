ed20bdb1fdbc2ea5651d46e905c84327b287464e
julianamonr03 <1967@holbertonschool.com>
2020-07-31 04:28:57 +0000

    Empty file

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+/**
+ * get_endianness - function that checks the endianness.
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	return (0);
+} 
