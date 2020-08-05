22d03344a5541c170d3ff9c89111ff50b36ae888
julianamonr03 <1967@holbertonschool.com>
2020-08-05 19:40:05 +0000

    fix

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,50 +1,50 @@
 #include "holberton.h"
 /**
- * main - program that copies the content of a file to another file
- * @ac: Number of arguments
- * @av: Arguments
+ * main - Program that copies the content of a file into another file
+ * @av: input arguments
+ * @ac: quantity of arguments
  * Return: 0 success
  */
 int main(int ac, char *av[])
 {
-	int file, file1, c, c1;
-	ssize_t r;
-	char buffer[2048];
+	int file, file1;
+	ssize_t r, w, c, c1;
+	char buffer[1024];
 
 	if (ac != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
-
 	file = open(av[1], O_RDONLY);
 	if (file == -1)
 	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't read from file %s\n", av[1]);
-		exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	}
 	file1 = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
 	if (file1 == -1)
 	{
-		dprintf(STDERR_FILENO,
-			"Error: Can't write to file 1 %s\n", av[2]);
-		exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
-	r = read(file, buffer, 1024);
-	write(file1, buffer, r);
-	c = close(file);
-
-	if (c == -1)
+	r = w = 1;
+	while (r)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file);
-		exit(100);
+		r = read(file, buffer, 1024);
+		if (r == -1)
+			dprintf(STDERR_FILENO,
+			"Error: Can't read from file %s\n", av[1]), exit(98);
+		if (r > 0)
+		{
+			w = write(file1, buffer, r);
+			if (w == -1)
+				dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", av[2]), exit(99);
+		}
 	}
+	c = close(file);
+	if (c == -1)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file), exit(100);
 	c1 = close(file1);
 	if (c1 == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file1);
-		exit(100);
-	}
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file1), exit(100);
 	return (0);
 }
