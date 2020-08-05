d7c6bb7107aab2e517578487e7777e45b07d976a
julianamonr03 <1967@holbertonschool.com>
2020-08-05 01:32:45 +0000

    Fix bug

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -18,18 +18,11 @@ int create_file(const char *filename, char *text_content)
 		for (; text_content[size_cont]; size_cont++)
 		;
 	}
-
 	open_new = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 
-	if (open_new == -1)
-	{
-		return (-1);
-	}
-
-	size_cont = 0;
 	write_new = write(open_new, text_content, size_cont);
 
-	if (write_new == -1)
+	if (write_new == -1 || open_new == -1)
 	{
 		return (-1);
 	}
