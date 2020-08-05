9463d84e8d11bd8a4e7952eb6e1876f50297961e
julianamonr03 <1967@holbertonschool.com>
2020-08-05 01:58:03 +0000

    Fix tabs

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,10 +16,9 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 		for (; text_content[size_cont]; size_cont++)
-		;
+			;
 	}
 	open_new = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
-
 	write_new = write(open_new, text_content, size_cont);
 
 	if (write_new == -1 || open_new == -1)
