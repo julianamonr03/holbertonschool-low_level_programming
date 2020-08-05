40f56e431448dc6d864ad34ea913d29525270b8f
julianamonr03 <1967@holbertonschool.com>
2020-08-05 00:25:49 +0000

    Create a file

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+/**
+ * create_file - Function that creates a file
+ * @filename: Name of the file to create
+ * @text_content: content
+ * Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int open_new, write_new, size_cont = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		for (; text_content[size_cont]; size_cont++)
+		;
+	}
+
+	open_new = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
+
+	if (open_new == -1)
+	{
+		return (-1);
+	}
+
+	size_cont = 0;
+	write_new = write(open_new, text_content, size_cont);
+
+	if (write_new == -1)
+	{
+		return (-1);
+	}
+	close(open_new);
+	return (1);
+}
