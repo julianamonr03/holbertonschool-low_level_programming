a64459b6dd85261227a4454b40372d0af631cb75
julianamonr03 <julianamonroy03@hotmail.com>
2020-09-06 22:44:00 +0000

    Doubly linked lists

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * print_dlistint -
+ * @h:
+ * Return: Number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+
+}
---
+++ 0x17-doubly_linked_lists/README.md
@@ -0,0 +1,6 @@
+## IN THIS PART WE ARE GONNA WORK WITH DOUBLY LINKED LISTS
+
+* How to use doubly linked lists
+
+-----
+*Juliana Monroy*
