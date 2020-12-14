2f610d6a058e7972328859f138c2df611bd96eda
julianamonr03 <julianamonroy03@hotmail.com>
2020-12-14 14:59:32 +0000

    Library

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -1,8 +1,5 @@
-#ifndef _LINKED_LIST_
-#define _LINKED_LIST_
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+#ifndef LISTS_H
+#define LISTS_H
 
 /**
  * struct dlistint_s - doubly linked list
@@ -20,6 +17,9 @@ typedef struct dlistint_s
     struct dlistint_s *next;
 } dlistint_t;
 
+#include <stdio.h>
+#include <stdlib.h>
+
 size_t print_dlistint(const dlistint_t *h);
 size_t dlistint_len(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
@@ -30,5 +30,4 @@ int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
 
-
-#endif /* _LINKED_LIST_ */
+#endif /* LISTS_H */
